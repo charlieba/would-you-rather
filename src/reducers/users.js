6d46ff169997cc027d2e2f0ae73793ed640f5468
Carlos Barillas <giovanib07@gmail.com>
2021-09-20 21:13:55 -0600

    feat: adding users actions and reducers

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,9 +1,9 @@
-import { RECEIVE_USERS } from '../actions/users';
+import { GET_USERS } from '../actions/users';
 import { SET_QUESTION, SET_ANSWER } from '../actions/questions';
 
 export default function users(state = {}, action) {
 	switch (action.type) {
-		case RECEIVE_USERS:
+		case GET_USERS:
 			return {
 				...state,
 				...action.users
