dd8d7e01a4b95db4ad6631588e1d5b1b24271478
Carlos Barillas <giovanib07@gmail.com>
2021-09-20 21:27:14 -0600

    feat: adding page not found

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 function PageNotFound() {
 	return (
 		<Fragment>
-			<h1 className="display3 text-center">404 ERROR</h1>
+			<h1 className="display3 text-center">Page Not Found</h1>
 			<h1 className="display4 text-center">
-				<Link to="/">Return to Home Page</Link>
+				<Link to="/">Go to Home</Link>
 			</h1>
 		</Fragment>
 	);
